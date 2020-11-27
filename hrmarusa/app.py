7cf41865d057ffc3b55284a75cbaf498c2a4edc6
ifserge <ifserge@gmail.com>
2020-11-27 20:49:40 +0300

    Update app.py

--- hrmarusa/app.py
+++ hrmarusa/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from genserv import generate_template
 app = Flask(__name__)
 
-
-@app.route('/generate')
+@app.route('/generate', methods=['GET', 'POST'])
 def genvac():
-    return {"description": "soon"}
+    json_data = flask.request.json
+    txt = json_data["text"]
+    return jsonify(generate_template(txt))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7000)
