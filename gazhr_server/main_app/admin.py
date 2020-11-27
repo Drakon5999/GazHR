d18014bddfc9f86bae08d0962511a35fae9b21fb
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-27 22:54:00 +0300

    Add admin and update gitignore

--- gazhr_server/main_app/admin.py
+++ gazhr_server/main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from gazht_server.models import *
 
 # Register your models here.
+
+admin.site.register(Scenario)
+admin.site.register(Task)
+admin.site.register(Vacancy)
+admin.site.register(Candidate)
+admin.site.register(Resume)
+admin.site.register(Vacancy2Resume)
