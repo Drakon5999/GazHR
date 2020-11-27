23d050c9d2e5ae038b1607b78ddb18a526ae4f54
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-27 20:32:25 +0300

    Fix syntax bug

--- hrmarusa/app.py
+++ hrmarusa/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/generate')
-def genvac:
-    return {"description":"soon"}
+def genvac():
+    return {"description": "soon"}
 
 if __name == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7000)
