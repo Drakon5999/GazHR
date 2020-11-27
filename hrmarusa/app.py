5af729fe4e0784d8e814187a02c8946d3210e903
sergey <sergeif@sergeys-MacBook-Pro.local>
2020-11-27 18:41:30 +0300

    initial

---
+++ hrmarusa/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/generate')
+def genvac:
+    return {"description":"soon"}
+
+if __name == '__main__':
+    app.run(debug=True, host='0.0.0.0')
