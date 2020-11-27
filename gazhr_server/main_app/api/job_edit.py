ff65e9952810c6bdc228a6247cb39989c5babb23
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 01:17:02 +0300

    Fix api

--- gazhr_server/main_app/api/job_edit.py
+++ gazhr_server/main_app/api/job_edit.py
@@ -8,12 +8,11 @@ import json
 def job_edit(request):
     try:
         data = json.loads(request.body)
-        job_id = data.pop("job_edit")
-        vacancy = Vacancy.object.filter("id__in"=[job_id])
+        job_id = data.pop("job_id")
+        vacancy = Vacancy.object.filter(id__in=[job_id])
         for key, value in data.items():
             setattr(vacancy, key, value)
         vacancy.save()
         return JsonResponse({"status": 200})
     except:
         return JsonResponse({"status": 404})
-
