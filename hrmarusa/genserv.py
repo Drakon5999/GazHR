43b474405c8b3e2ad476cb13f1dcae4056e40c2d
sergey <sergeif@sergeys-MacBook-Pro.local>
2020-11-27 20:58:15 +0300

    fixed

--- hrmarusa/genserv.py
+++ hrmarusa/genserv.py
@@ -16,27 +16,30 @@ def generate_template(txt):
     sentences = txt.split('.')
     type_of_resume = 0
     for sk in skill_entities_programer:
-        if txt.find(sk) > 0:
+        if txt.lower().find(sk) > 0:
             type_of_resume = 1
             break
     template = read_template(type_of_resume)
     new_texts = []
+    experience = []
+    skills = []
     for s in sentences:
         dont_include = False
-        skills = []
+        
         for sk in skill_entities_programer:
-            if txt.find(sk) > 0:
+            if s.lower().find(sk) > 0:
                 skills.append(sk)
                 dont_include = True
+
         for sk in skill_entities_modeler:
-            if txt.find(sk) > 0:
+            if s.lower().find(sk) > 0:
                 skills.append(sk)
                 dont_include = True
-        experience = []
-        for s in year_entities:
-            if len(re.findall(r""+year_entities, s)) > 0:
+        
+        for ys in year_entities:
+            if len(re.findall(r"("+ys+")", s)) > 0:
                 dont_include = True
-                for q in re.finall(r"("+year_entities+")", s):
+                for q in re.findall(r"("+ys+")", s):
                     experience.append(q)
         if not dont_include:
             new_texts.append(s)
