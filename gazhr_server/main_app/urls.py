cfeefe43475eec91e6be0ad0f419e167cf7ac399
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-27 22:45:59 +0300

    Add get_jobs_list api

--- gazhr_server/main_app/urls.py
+++ gazhr_server/main_app/urls.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonRespons
 from django.urls import path, reverse
 from django.shortcuts import render
 
+from main_app.api.get_jobs_list import get_jobs_list
 
 urlpatterns = [
+    path("api/get_jobs_list", get_jobs_list, name="api/get_jobs_list"),
 ]
