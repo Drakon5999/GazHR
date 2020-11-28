2423461ecd4195e7b1ef0ed51fd217e98f7f27f3
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 16:42:40 +0300

    Update job_edit

--- gazhr_server/main_app/api/job_edit.py
+++ gazhr_server/main_app/api/job_edit.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
-from main_app.models import Vacancy
+from main_app.models import Vacancy, Scenario
 
 
 import json
@@ -14,7 +14,11 @@ def job_edit(request):
         job_id = data.pop("job_id")
         vacancy = Vacancy.objects.filter(id__in=[job_id]).get()
         for key, value in data.items():
-            setattr(vacancy, key, value)
+            if key == "scenario_id":
+                s = Scenario.objects.get(id=value)
+                setattr(vacancy, key, s)
+            else:
+                setattr(vacancy, key, value)
         vacancy.save()
         return JsonResponse({"status": 200})
     except BaseException as e:
