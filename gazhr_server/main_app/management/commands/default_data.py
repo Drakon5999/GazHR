e56c276840fcd4506e3d539c1250b507f02a6b66
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 18:02:14 +0300

    FIx scenatio

--- gazhr_server/main_app/management/commands/default_data.py
+++ gazhr_server/main_app/management/commands/default_data.py
@@ -75,7 +75,22 @@ class Command(BaseCommand):
         s1 = Scenario(
             created_timestamp=datetime.datetime.now(),
             json_scenario={
-                1: "Реши упражнение 1"
+                "data": [{
+                    "step_name": "\u0420\u0430\u0441\u043c\u043e\u0442\u0440\u0435\u043d\u0438\u0435 \u0440\u0435\u0437\u044e\u043c\u0435",
+                    "text_deny": "\u041f\u043e\u043a\u0430 \u043f\u043e\u043a\u0430",
+                    "is_meeting": False,
+                    "text_next_step": "\u041f\u0440\u0438\u0432\u0435\u0442"
+                }, {
+                    "step_name": "\u0422\u0435\u0441\u0442\u043e\u0432\u043e\u0435 \u0437\u0430\u0434\u0430\u043d\u0438\u0435",
+                    "text_deny": "\u0427\u0442\u043e-\u0442\u043e \u043d\u0435 \u043e\u0447\u0435\u043d\u044c",
+                    "is_meeting": False,
+                    "text_next_step": "\u041e \u0441\u043d\u043e\u0432\u0430 \u0442\u044b"
+                }, {
+                    "step_name": "\u0421\u043e\u0431\u0435\u0441\u0435\u0434\u043e\u0432\u0430\u043d\u0438\u0435",
+                    "text_deny": "\u041d\u0443 \u0442\u0430\u043a \u0441\u0435\u0431\u0435",
+                    "is_meeting": True,
+                    "text_next_step": "\u041f\u0440\u0438\u0432\u0435\u0442 \u0438 \u0434\u043e\u0431\u0440\u043e \u043f\u043e\u0436\u0430\u043b\u043e\u0432\u0430\u0442\u044c"
+                }]
             }
         )
         s1.save()
