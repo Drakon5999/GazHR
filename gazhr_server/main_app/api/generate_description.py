44d832b7d2c9ee90d801786359211518c6d383ad
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 15:36:31 +0300

    reverse

--- gazhr_server/main_app/api/generate_description.py
+++ gazhr_server/main_app/api/generate_description.py
@@ -5,7 +5,6 @@ from main_app.models import Vacancy
 import json
 import requests
 from django.views.decorators.csrf import csrf_exempt
-import datetime
 
 
 @csrf_exempt
@@ -14,15 +13,7 @@ def generate_description(request):
         data = json.loads(request.body)
         model_response = requests.post(
             'http://{}:{}/generate'.format(settings.MODEL_HOST, settings.MODEL_PORT), json=data).json()
-        vacancy = Vacancy(
-            source_text=data["text"],
-            name=model_response["description"],
-            transformed_data=model_response,
-            created_timestamp=datetime.datetime.now(),
-            finished_timestamp=datetime.datetime.now(),
-            transformed_text=model_response["hh"]["description"],
-        )
-        vacancy.save()
+
         return JsonResponse({"status": 200, "data": model_response})
     except BaseException as e:
         return JsonResponse({"status": 404, "error": str(e)})
