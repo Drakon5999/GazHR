f52f8642f8d27a22ecbb34f190006731e9f521be
Grigorii Korepanov <grigoriikorepanov@gmail.com>
2020-11-28 03:24:18 +0300

    fixed

--- gazhr_front/src/containers/index.js
+++ gazhr_front/src/containers/index.js
@@ -1,9 +1,11 @@
 import GeneratedText from './GeneratedText';
 import TextEditor from './TextEditor';
 import VacancyMinimized from './VacancyMinimized';
+import VacancyCandidates from './VacancyCandidates';
 
 export {
   GeneratedText,
   TextEditor,
   VacancyMinimized,
+  VacancyCandidates,
 }
