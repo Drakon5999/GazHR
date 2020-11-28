ac71ed03747179c52b11461b16a09f8e00c21a66
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 13:31:31 +0300

    remove garbege

--- gazhr_server/main_app/default_data.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def create_scenario(apps, schema_editor):
-    Scenario = apps.get_model("main_app", "Scenario")
-    s1 = Scenario()
-    s1.json_scenario = {}
-    s1.created_timestamp = datetime.datetime.now().timestamp()
-    s1.save()
