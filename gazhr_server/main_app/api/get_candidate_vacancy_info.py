f2cb37adf42981cfd307f532e8875c3718979def
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 14:41:53 +0300

    FIx get_candidate_vacancy_info

--- gazhr_server/main_app/api/get_candidate_vacancy_info.py
+++ gazhr_server/main_app/api/get_candidate_vacancy_info.py
@@ -11,10 +11,10 @@ def get_candidate_vacancy_info(request):
         data = json.loads(request.body)
         vacancy = Vacancy.objects.get(id=data['vacancy_id'])
         candidate = Candidate.objects.get(id=data['candidate_id'])
-        vac2can = Vacancy2Resume.objects.filter(vacancy_id=data['vacancy_id'])
+        vac2can = Vacancy2Resume.objects.filter(vacancy_id=vacancy)
         for x in vac2can:
-            res = Resume.objects.get(x.resume_id)
-            if res.candidate_id == data['candidate_id']:
+            res = Resume.objects.get(id=x.resume_id.id)
+            if res.candidate_id.id == data['candidate_id']:
                 ans = {"name": candidate.full_name, "score": res.score, "status": res.status,
                        "additional_json": candidate.addition_info}
                 return JsonResponse({"status": 200, "data": json.dumps(ans)})
