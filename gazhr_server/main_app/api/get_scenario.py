cb5a857359404db7b9aa3f747bde2c48b75cfa76
Skiba Gleb <g.skiba@corp.mail.ru>
2020-11-28 18:44:00 +0300

    fix scenario

--- gazhr_server/main_app/api/get_scenario.py
+++ gazhr_server/main_app/api/get_scenario.py
@@ -10,6 +10,6 @@ def get_scenario(request):
     try:
         data = json.loads(request.body)
         s = Scenario.objects.get(id=data['scenario_id'])
-        return JsonResponse({"status": 200, "data": s.json_scenario["data"]})
+        return JsonResponse({"status": 200, "data": s.json_scenario})
     except BaseException as e:
         return JsonResponse({"status": 404, "error": str(e)})
