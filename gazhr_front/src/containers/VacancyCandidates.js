b230cfc5686aae565c1f154ea113b2ce3d9c946b
Grigorii Korepanov <grigoriikorepanov@gmail.com>
2020-11-28 03:48:51 +0300

    asda

--- gazhr_front/src/containers/VacancyCandidates.js
+++ gazhr_front/src/containers/VacancyCandidates.js
@@ -1,11 +1,18 @@
-import {Card, ListGroup} from 'react-bootstrap';
+import {Card, ListGroup, Button} from 'react-bootstrap';
 
 function VacancyCandidates({candidates}) {
   return (
     <Card style={{width: '100%', height: '100%'}}>
       <Card.Header>Список кандидатов</Card.Header>
       <ListGroup variant="flush">
-        {candidates.map(candidate => <ListGroup.Item key={candidate}>{candidate}</ListGroup.Item>)}
+        {candidates.map(candidate => (
+          <ListGroup.Item key={`${candidate.score}${candidate.name}`}>
+            <span>{candidate.name}</span>
+            <span>{candidate.score}</span>
+            <Button>V</Button>
+            <Button>X</Button>
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </Card>
   );
