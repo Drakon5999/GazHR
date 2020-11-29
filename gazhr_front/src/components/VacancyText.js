55513db036420ef90df2f83093eb528cea04e8d6
Ivan <i.v.a.n_l@mail.ru>
2020-11-29 07:34:46 +0300

    fix whitespaces in vacancies

--- gazhr_front/src/components/VacancyText.js
+++ gazhr_front/src/components/VacancyText.js
@@ -7,7 +7,7 @@ function VacancyText({text}) {
       <h4>Что нужно уметь и что мы хотим получить</h4>
 
       <Card bg="light" className="GeneratedText">
-      <Card.Body>
+      <Card.Body style={{whiteSpace: "break-spaces"}}>
         <Card.Text className="text-left">
           {text || 'Текст вакансии'}
         </Card.Text>
